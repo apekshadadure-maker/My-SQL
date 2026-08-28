eca796d50b7aebd589f41c378a1e2316da33612e
Apeksha <apekshadadure@gmail.com>
2026-08-28 08:10:54 +0530

    database create successfully

---
+++ Day 2.sql
@@ -0,0 +1,28 @@
+Create database bankingdb;
+
+use bankingdb;
+
+Create table IF NOT EXISTS Customers(
+    CustomerID int, FirstName varchar(50),
+    LastName varchar(50), Email varchar(100),
+    Phone varchar(20)
+    );
+
+desc customer;
+
+-- to add new column 'AccountCreation'--->DATE--
+alter table customers
+add AccountCreationDate date;
+
+insert into Customers(
+CustomerId,FirstName,LastName,Email,Phone,AccountCreationDate)
+values(101,'Raj','Kuver','raj_k@gmail.com',9881004242,'2025-10-25');
+
+-- to retrieve data from table--
+-- syntax; Select*from <table_name>;--
+select*from Customers;
+
+select FirstName,Email,AccountCreationDate
+from Customers;
+bu
+
